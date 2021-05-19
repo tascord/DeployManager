18fbfc6053969a233edc8cd0d8b80ce93324edb5
tascord <theactivesquare@gmail.com>
2021-05-19 22:28:42 +1000

    how

--- src/lib.js
+++ src/lib.js
@@ -89,7 +89,7 @@ class WebImplementation extends require('events').EventEmitter {
             console.log(`Looking for open port above ${lower_bound}.`)
 
             console.log(`Looking for services running on ${port}.`);
-            console.log(get_services(), get_services().find(s => s.port === port))
+            console.log(services.all().map(s => services.get(s.ID).port).find(p => p === port))
 
             if (error) throw new Error(`Error sweeping ports: ${error}`);
             if (get_services().find(s => s.port === port)) {
